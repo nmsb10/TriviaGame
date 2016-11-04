b5d5fa91cc5642ed30fd065f9f03c037d8ae9a6f
Jon B <Jon B>
2016-11-04 03:58:26 -0500

    homework 5 updated, darn timers are screwy

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,38 @@
 //remember: first global variables, then objects, then calls
+//WHY does game.qaOriginal get empty after all questions are asked???
+var qaOOO = [
+		{
+			question: "question one here",
+			answersIncorrect: ["wrong 1","wrong 2","wrong 3"],
+			answer: "answer to question one here.",
+			explanation: "This answer makes sense because explanation."
+		},
+		{
+			question: "question two here",
+			answersIncorrect: ["wrong 1","wrong 2","wrong 3","wrong 4"],
+			answer: "answer to question two here.",
+			explanation: "Answer is correct because explanation."
+		},
+		{
+			question: "question three here",
+			answersIncorrect: ["false"],
+			answer: "true",
+			explanation: "This is true because explanation."
+		},
+		{
+			question: "question four here",
+			answersIncorrect: ["wrong 1","wrong 2","wrong 3"],
+			answer: "answer to question four here.",
+			explanation: "This answer is correct because explanation."
+		},
+		{
+			question: "question five here",
+			answersIncorrect: ["wrong 1","wrong 2"],
+			answer: "answer to question five here.",
+			explanation: "Answer makes sense because explanation."
+		}
+	];
+
 var game =
 {
 	qaOriginal:
@@ -46,15 +80,29 @@ var game =
 	userSelection:"",
 	percentageCorrect:0,
 	questionTimer:0,
-	timerAnswer:5,
-	counter:0,
+	answerTimer:0,
+	// counter:0,
 	answeredQuestion:false,
 	//http://www.w3schools.com/jsref/met_win_setinterval.asp
 	timeTheUserOnQuestion: function(){
-		game.counter = setInterval(game.decrement, 1000);
+		counter = setInterval(game.decrement, 1000);
+	},
+	timedAnswerReveal: function(){
+		counter = setInterval(game.answerReveal, 1000);
+	},
+	answerReveal: function(){
+		game.answerTimer --;
+		if(game.answerTimer === 0){
+			game.setQuestionTimer();
+			game.generateQuestion();
+			game.timeTheUserOnQuestion();
+		}
 	},
 	setQuestionTimer:function(){
-		game.questionTimer = 21;
+		game.questionTimer = 5;
+	},
+	setAnswerTimer: function(){
+		game.answerTimer = 5;
 	},
 	decrement: function(){
 		game.questionTimer --;
@@ -69,13 +117,18 @@ var game =
 			$("#answers").html("<div class='text'>" + game.incorrectAnswerResponse[Math.floor(Math.random()*game.incorrectAnswerResponse.length)] + "</div>");
 			$("#answers").append("<div class='text'>" + game.qa[game.currentSelection].answer + "</div>");
 			$("#answers").append("<div class='text'>" + game.qa[game.currentSelection].explanation + "</div>");
+			game.qa.splice(game.currentSelection,1);
+			game.setAnswerTimer();
+			game.timedAnswerReveal();
 		}
 		if(game.answeredQuestion===true){
 			game.stop();
 		}
 	},
 	stop: function(){
-		clearInterval(game.counter);
+		console.log("stop function used!");
+		clearInterval(counter);
+		console.log(counter);
 	},
 	questionsRemaining: function(){
 		//first check if game.qa array is empty. if empty, show final page
@@ -84,55 +137,89 @@ var game =
 		if(game.qa.length===0){
 			return false;
 		}
+		else{
+			return true;
+		}
 	},
 	generateQuestion: function(){
-		// game.questionsRemaining();
-		// game.timeTheUserOnQuestion();
-		//randomly choose a question from remaining elements in the game.qa array
-		game.currentSelection = Math.floor(Math.random()*game.qa.length);
-		game.currentQuestion = game.qa[game.currentSelection].question;
-		//choose the random array element location of (game.qa[game.currentSelection].answersIncorrect.length+1) answers for the correct answer
-		game.correctAnswerPlace = Math.floor(Math.random()*(game.qa[game.currentSelection].answersIncorrect.length + 1));
-		console.log("correct answer place is " + game.correctAnswerPlace);
-		console.log("current answer array is " + game.currentAnswers);
-		var totalAnswerCount = game.qa[game.currentSelection].answersIncorrect.length + 1;
-		var currentAnswers = [];
-		//populate currentAnswers array with answers randomly presented
-		//WHY will statement 2 in this for loop not take the content of the totalAnswerCount variable instead???
-		for(var i = 0; i < totalAnswerCount; i++){
-			console.log(i);
-			var chooseRandomWrongAnswer = 0;
-			if(i === game.correctAnswerPlace){
-				currentAnswers.push(game.qa[game.currentSelection].answer);
+		console.log(game.questionsRemaining());
+		game.answeredQuestion = false;
+		if(game.questionsRemaining()){
+			// game.timeTheUserOnQuestion();
+			//randomly choose a question from remaining elements in the game.qa array
+			game.currentSelection = Math.floor(Math.random()*game.qa.length);
+			game.currentQuestion = game.qa[game.currentSelection].question;
+			//choose the random array element location of (game.qa[game.currentSelection].answersIncorrect.length+1) answers for the correct answer
+			game.correctAnswerPlace = Math.floor(Math.random()*(game.qa[game.currentSelection].answersIncorrect.length + 1));
+			console.log("correct answer place is " + game.correctAnswerPlace);
+			console.log("current answer array is " + game.currentAnswers);
+			var totalAnswerCount = game.qa[game.currentSelection].answersIncorrect.length + 1;
+			var currentAnswers = [];
+			//populate currentAnswers array with answers randomly presented
+			//WHY will statement 2 in this for loop not take the content of the totalAnswerCount variable instead???
+			for(var i = 0; i < totalAnswerCount; i++){
+				console.log(i);
+				var chooseRandomWrongAnswer = 0;
+				if(i === game.correctAnswerPlace){
+					currentAnswers.push(game.qa[game.currentSelection].answer);
+				}
+				else{
+					chooseRandomWrongAnswer = Math.floor(Math.random()*game.qa[game.currentSelection].answersIncorrect.length);
+					console.log(chooseRandomWrongAnswer);
+					currentAnswers.push(game.qa[game.currentSelection].answersIncorrect[chooseRandomWrongAnswer]);
+					console.log(game.qa[game.currentSelection].answersIncorrect);
+					game.qa[game.currentSelection].answersIncorrect.splice(chooseRandomWrongAnswer,1);
+				}
+				console.log("current answer array is " + currentAnswers);
 			}
-			else{
-				chooseRandomWrongAnswer = Math.floor(Math.random()*game.qa[game.currentSelection].answersIncorrect.length);
-				console.log(chooseRandomWrongAnswer);
-				currentAnswers.push(game.qa[game.currentSelection].answersIncorrect[chooseRandomWrongAnswer]);
-				console.log(game.qa[game.currentSelection].answersIncorrect);
-				game.qa[game.currentSelection].answersIncorrect.splice(chooseRandomWrongAnswer,1);
+			$("#question").html(game.currentQuestion);
+			var choices = "";
+			for(var i = 0; i<currentAnswers.length; i++){
+				choices += "<div class='text possible-answer' id='"+ i + "'>" + currentAnswers[i] + "</div>";
+				//would this work??
+				//$("#answers").append(choices);
+				console.log(choices);
 			}
-			console.log("current answer array is " + currentAnswers);
+			$("#answers").html(choices);
+		} else {
+			game.stop();
+			//after all questions completed, final page says, "here's how you did:"
+			//display userCorrectAnswers, userIncorrectAnswers, userUnanswered, percentageCorrect=correct/totalnumberofquestions
+			//display "start over" button
+			//start over button restarts at ga.qaOriginal (as game.qa has been depleted to nothing)
+			//reset usercorrect and incorrect response counts, etc.
+			$("#question").html("Congratulations. You completed the \"Renters: Working with a Real Estate Broker\" quiz.");
+			$("#answers").html("<div class='text'>" + "Here's how you did:" + "</div>");
+			$("#answers").append("<div class='text'> Correct Answers: " + game.userCorrectAnswers + "</div>");
+			$("#answers").append("<div class='text'> Incorrect Answers: " + game.userIncorrectAnswers + "</div>");
+			$("#answers").append("<div class='text'> Questions not answered: " + game.userUnansweredAnswers + "</div>");
+			game.percentageCorrect = ((game.userCorrectAnswers *100) / (game.userCorrectAnswers + game.userIncorrectAnswers + game.userUnansweredAnswers)).toFixed(2);
+			$("#answers").append("<div class='text'> Questions answered correctly: " + game.percentageCorrect + "%</div>");
+			$("#answers").append("<button id='start-button' class='start-over'>" + "start over" + "</button>");
+			game.startOver();
 		}
-		$("#question").html(game.currentQuestion);
-		var choices = "";
-		for(var i = 0; i<currentAnswers.length; i++){
-			choices += "<div class='text possible-answer' id='"+ i + "'>" + currentAnswers[i] + "</div>";
-			//would this work??
-			//$("#answers").append(choices);
-			console.log(choices);
-		}
-		$("#answers").html(choices);
-
-		// $("#answer1").html(currentAnswers[0]);
-		// $("#answer2").html(currentAnswers[1]);
-		// $("#answer3").html(currentAnswers[2]);
-		// $("#answer4").html(currentAnswers[3]);
-		// $("#answer5").html(currentAnswers[4]);
+	},
+	startOver:function(){
+		$(".start-over").on("click",function(){
+			console.log(qaOOO);
+			game.qa = qaOOO;
+			console.log(game.qa);
+			$("#timer").html("timer");
+			$("#question").html("");
+			$("#answers").html("");
+			game.userCorrectAnswers = 0;
+			game.userIncorrectAnswers = 0;
+			game.userUnansweredAnswers = 0;
+			game.setQuestionTimer();
+			game.generateQuestion();
+			game.timeTheUserOnQuestion();
+		});
 	},
 	checkResponse:function(){
 		$(".possible-answer").on("click",function(){
-			game.answeredQuestion = true;
+			game.stop();
+			game.answeredQuestion = true;//this stops the timer if user picks an answer
+			console.log("user clicked on an answer? " + game.answeredQuestion);
 			console.log("id of this possible answer is " + $(this).attr("id"));
 			console.log("correct answer place is " + game.correctAnswerPlace);
 			//if user selection is correct:
@@ -156,15 +243,18 @@ var game =
 			console.log(game.qa.length);
 			game.qa.splice(game.currentSelection,1);
 			console.log("number of possible questions left is " + game.qa.length);
+			game.setAnswerTimer();
+			game.timedAnswerReveal();
 		});
 	}
-}
+};
 
 $(document).ready(function(){
 	$("#start-button").on("click",function(){
 		$("#start-button").hide();
 		game.qa = game.qaOriginal;
 		// game.questionsRemaining();
+		$("#timer").html("timer");
 		game.setQuestionTimer();
 		game.generateQuestion();
 		game.timeTheUserOnQuestion();
@@ -191,9 +281,3 @@ $(document).ready(function(){
 //display (random) incorrectAnswerResponse, and below say, "The correct answer is" + answer. Timer for 5 seconds on this page.
 
 //after 5 seconds, load next question automatically and reset timer to 30 seconds
-
-//after all questions completed, final page says, "here's how you did:"
-//display userCorrectAnswers, userIncorrectAnswers, userUnanswered, percentageCorrect=correct/totalnumberofquestions
-//display "start over" button
-//start over button restarts at ga.qaOriginal (as game.qa has been depleted to nothing)
-//reset usercorrect and incorrect response counts, etc.
