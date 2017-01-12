493bd5f2af607eb72dbad797615f1a898782081f
nmsb10 <j-nagatani@u.northwestern.edu>
2017-01-11 18:03:11 -0600

    some updates

--- assets/javascript/app02.js
+++ assets/javascript/app02.js
@@ -3,7 +3,9 @@
 document.addEventListener("DOMContentLoaded", function(event) {
 	//http://stackoverflow.com/questions/799981/document-ready-equivalent-without-jquery
 	showDate();
-	submitBet();
+	updateRollButton();
+	enterRequest();
+	roll();
 });
 
 function showDate(){
@@ -40,33 +42,268 @@ var dateFun = {
 	}
 };
 
-var clicks = 0;
-function submitBet(){
-	console.log('submitBid function called');
+var rollStatus = {
+	comeOut: true,
+	point:0,
+	rollNumber:0
+};
+
+function Bet(type, amount, number){
+	this.type = type;
+	this.amount = amount;
+	this.number = number;
+}
+
+function MultiwordBet(){
+	this.bet = false;
+	this.pass = false;
+	this.passline = false;
+	this.line = false;
+	this.do = false;
+	this.not = false;
+	this.dont = false;
+	this.place = false;
+	this.odds = false;
+	this.lay = false;
+	this.come = false;
+}
+
+function updateRollButton(){
+	if(rollStatus.comeOut){
+		document.getElementById('roll-description').innerText = 'come out';
+		document.getElementById('point-number').innerText = 'no point';
+	}else{
+		document.getElementById('roll-description').innerText = 'point is';
+		document.getElementById('point-number').innerText = rollStatus.point;
+	}
+}
+
+function enterRequest(){
 	document.getElementById('add-bet-button').addEventListener('click', function(event){
-		clicks ++;
-		console.log('your clicks: ' + clicks);
 		var bet = document.getElementById('input-bet').value;
-		console.log(bet);
+		document.getElementById('input-bet').value = '';
+		//evaluate the request
+		var requestArray = bet.split(' ');
+		var numbersArray = [];
+		var stringsArray = [];
+		//separate the request into an array of strings, and a separate array of numbers
+		for(var i = 0; i<requestArray.length; i++){
+			//parseFloat?
+			if(parseInt(requestArray[i])){
+				numbersArray.push(requestArray[i]);
+			}else{
+				stringsArray.push(requestArray[i]);
+			}
+		}
+		var currentBet = new Bet();
+		if(stringsArray.length === 1){
+			switch(stringsArray[0]){
+				case 'pass':
+				case 'passline':
+				case 'pl':
+					currentBet.type = 'passline';
+					break;
+				case 'don\'tpass':
+				case 'dontpass':
+				case 'dp':
+					currentBet.type = 'donotpassline';
+					break;
+				case 'place':
+					currentBet.type = 'place';
+					break;
+				case 'odds'://note if user only specifies odds, it is assumed this is for a pass line bet, and not laying odds or placing odds on a come bet
+				case 'placeodds':
+					currentBet.type = 'placeodds';
+					break;
+				case 'lo':
+				case 'layodds':
+					currentBet.type = 'layodds';
+					break;
+				case 'come':
+				case 'comebet':
+					currentBet.type = 'come';
+					break;
+				case 'dc':
+				case 'dontcome':
+					currentBet.type = 'dontcome';
+					break;
+				default:
+					console.log('unknown bet TYPE');
+					break;
+			}
+		}else if (stringsArray.length > 1){
+			//you must have a multiword bet if the strings array length is > 1
+			var mwbet = new MultiwordBet();
+			//check if any of the words is a part of the possible multi-word bet words from the options in the MultiwordBet constructor
+			for(var j = 0; j < stringsArray.length; j++){
+				switch(stringsArray[j]){
+					case 'bet':
+						mwbet.bet = true;
+						break;
+					case 'pass':
+						mwbet.pass = true;
+						break;
+					case 'passline':
+						mwbet.passline = true;
+						break;
+					case 'line':
+						mwbet.line = true;
+						break;
+					case 'do':
+						mwbet.do = true;
+						break;
+					case 'not':
+						mwbet.not = true;
+						break;
+					case 'dont':
+					case 'don\'t':
+						mwbet.dont = true;
+						break;
+					case 'place':
+						mwbet.place = true;
+						break;
+					case 'odds':
+						mwbet.odds = true;
+						break;
+					case 'lay':
+						mwbet.lay = true;
+						break;
+					case 'come':
+						mwbet.come = true;
+						break;
+					default:
+						console.log('unknown WORD used in bet request: '+ stringsArray[j]);
+						break;
+				}
+			}
+			console.log(mwbet);
+			//evaluate mwbet to properly assign currentBet.type
+			//should I have a return after each else if statement?
+			for(var key in mwbet){
+				console.log('searching mwbet now!!!');
+				if((mwbet.do && mwbet.not && mwbet.pass) || (mwbet.dont && mwbet.pass) || (mwbet.dont && mwbet.pass && mwbet.bet) || (mwbet.do && mwbet.not && mwbet.passline) || (mwbet.do && mwbet.not && mwbet.passline && mwbet.bet)){
+					currentBet.type = 'donotpassline';
+				}else if((mwbet.pass && mwbet.line) || (mwbet.passline && mwbet.bet) || (mwbet.pass && mwbet.line && mwbet.bet)){
+					//MUST check for a "plain" pass line bet AFTER don't pass
+					currentBet.type = 'passline';
+				}else if(mwbet.place && mwbet.odds){
+					currentBet.type = 'placeodds';
+				}else if(mwbet.lay && mwbet.odds){
+					currentBet.type = 'layodds';
+				}else if((mwbet.dont && mwbet.come) || (mwbet.dont && mwbet.come && mwbet.bet) || (mwbet.do && mwbet.not && mwbet.come) || (mwbet.do && mwbet.not && mwbet.come && mwbet.bet)){
+					currentBet.type = 'dontcome';
+				}else if(mwbet.come && mwbet.bet){//MUST check for a "plain" come bet AFTER don't come
+					currentBet.type = 'come';
+				}else if(mwbet.place && mwbet.bet){
+					currentBet.type = 'place';
+				}
+			}
+		}
+		//evaluate for the number and bet amount
+		if(numbersArray.length === 1){
+			currentBet.amount = numbersArray[0];
+		}else{
+			currentBet.amount = numbersArray[1];
+			currentBet.number = numbersArray[0];
+		}
+		//evaluate the constructor using the submitBet function
+		//submitBet(currentBet.type, currentBet.amount, currentBet.number);
+		submitBet(currentBet);
 		document.getElementById('input-bet').value = '';
 		//return false;
 		event.preventDefault();
 	});
 }
 
+function roll(){
+	document.getElementById('roll-button').addEventListener('click', function(event){
+		rollStatus.rollNumber ++;
+		var dice = {
+			one: Math.floor(Math.random()*6)+1,
+			two: Math.floor(Math.random()*6)+1
+		};
+		var twoDiceDisplay = "<div class='two-dice'><div class='dice-result'>" + dice.one + "</div><div class='dice-result'>" + dice.two + "</div></div>";
+		document.getElementById('roll-results').innerHTML += twoDiceDisplay;
+		resolveBets(dice.one, dice.two);
+		updateRollStatus(dice.one, dice.two);
+		updateRollButton();
+		// event.preventDefault();
+	});
+}
+
+function resolveBets(one, two){
+
+}
+
+function updateRollStatus(dieOne, dieTwo){
+	var rollTotal = dieOne + dieTwo;
+	if(rollStatus.comeOut){
+		if(rollTotal === 7 || rollTotal === 11){
+			console.log('front line winner');
+		}else if (rollTotal === 2 || rollTotal === 3 || rollTotal === 12){
+			console.log('front line loses');
+		}else{
+			console.log('point is ' + rollTotal);
+			rollStatus.point = rollTotal;
+			rollStatus.comeOut = false;
+		}
+	}else{
+		if(rollTotal===7){
+			console.log('pass line loses.');
+			rollStatus.comeOut = true;
+		}else if(rollTotal === rollStatus.point){
+			console.log('pass line wins.');
+			rollStatus.comeOut = true;
+		}
+	}
+}
+
+function submitBet(bet){
+	console.log(bet.type);
+	switch(bet.type){
+		case 'passline':
+			console.log('passline bet');
+			break;
+		case 'donotpassline':
+			console.log('don\'t pass bet');
+			break;
+		case 'placeodds':
+			console.log('place odds bet');
+			break;
+		case 'layodds':
+			console.log('lay odds bet');
+			break;
+		case 'come':
+			console.log('come bet');
+			break;
+		case 'dontcome':
+			console.log('don\'t come bet');
+			break;
+		case 'place':
+			console.log('place bet');
+			break;
+		default:
+			console.log('unknown bet type');
+			break;
+	}
+}
+
 
 //answer for keeping the chat thing scrolled to bottom unless user scrolls otherwise
 	//http://stackoverflow.com/questions/18614301/keep-overflow-div-scrolled-to-bottom-unless-user-scrolls-up
 
-
-
-
-
+//document.getElementsByClassName("example");
 //.value, .innerHTML
 //document.queryselector??
 //use a switch statement??
 //functions that return values??
 //indexOf (returns -1 if not in there)
+
+//add class to an element:
+// var d = document.getElementById("div1");
+// d.className += " otherclass"; NB the space before the otherclass name
+
+
 
 // arrayName.indexOf("elementInTheArray") = returns the
 // index number of that element
